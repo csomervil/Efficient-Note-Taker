10d2f89790563a9efebcd44c440a1ded5c3bbd7a
csomervil <carson.somerville@mail.utoronto.ca>
2022-05-04 14:16:24 -0400

    now updates with addition of new note

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,15 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./db/db.json');
 const fs = require('fs');
+const { channel } = require('diagnostics_channel');
 const PORT = 3001;
+let channels = [];
+
+randomNumber = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
 
 const app = express();
 
@@ -16,36 +24,44 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
   });
 
-
 // app.get('/notes', (req, res) => {
 //   res.json(`${req.method} request received to get reviews`)
 // });
 
 app.get('/api/notes', (req, res) => {
-  res.json(noteData);
+  console.log(channels)
+  console.log(noteData)
 
   console.info(`${req.method} request received to get reviews`);
+  if (channels.length < 1) {
+    return res.send(noteData);
+  }
+  return res.json(channels);
+  
 });
 
 
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
-
-  const { title, text } = req.body;
-  if (title && text) {
+  if (channels === []) {
+    channels = noteData;
+  }
+  const { title, text, id } = req.body;
+  if (title && text && id) {
     const newNote = {
       title,
       text,
+      id: randomNumber(),
     };
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
-        const parsedNotes = JSON.parse(data);
-        parsedNotes.push(newNote);
+        channels = JSON.parse(data);
+        channels.push(newNote);
         fs.writeFile(
           './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
+          JSON.stringify(channels, null, 4),
           (writeErr) =>
             writeErr ? console.error(writeErr)
             : console.info('Successfully updated notes!')
@@ -62,6 +78,22 @@ app.post('/api/notes', (req, res) => {
     res.json('Error in posting note');
   }
 });
+
+// app.post('/notes', (req, res) => {
+//   console.log(noteData);
+// });
+
+// app.delete('/api/notes/:id', (req, res) => {
+//   const { id } = req.params;
+//   const deleted = channels.find(channel => channel.id === id);
+//   if (deleted) {
+//     channels = channels.filter(channel => channel.id != id);
+//     res.json(deleted);
+//   } else {
+//     res.json( { message: "No" })
+//   }
+
+// });
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
